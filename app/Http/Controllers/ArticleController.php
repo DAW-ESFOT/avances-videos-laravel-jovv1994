d3f33e98add417be3850bb1b44cb04a4f31078f8
John Vásconez <jovv1994@outlook.com>
2020-11-29 16:17:58 -0500

    #10 Cors

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -21,13 +21,12 @@ class ArticleController extends Controller
         return response()->json($article, 201);
     }
     public function update(Request $request, Article $article)
-    {Article $article
+    {
         $article->update($request->all());
         return response()->json($article, 200);
     }
-    public function delete(Request $request, $id)
+    public function delete(Article $article)
     {
-        $article = Article::findOrFail(Article $article);
         $article->delete();
         return response()->json(null, 204);
     }
