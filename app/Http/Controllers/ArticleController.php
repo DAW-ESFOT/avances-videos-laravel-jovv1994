89cd2bcb10572c6660a741f1b2c3dc7a6cc733ed
John Vásconez <jovv1994@outlook.com>
2021-01-22 23:17:57 -0500

    #17 Validaciones

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -13,11 +13,13 @@ class ArticleController extends Controller
     private static $rules = [
         'title' => 'required|string|unique:articles|max:255',
         'body' => 'required',
+        'category_id' => 'required|exists:categories,id'
     ];
     private static $messages = [
         'required' => 'El campo :attribute es obligatorio.',
         'body.required' => 'El body no es valido.',
     ];
+
     public function index()
     {
         return new ArticleCollection(Article::paginate());
@@ -31,14 +33,11 @@ class ArticleController extends Controller
     public function store(Request $request)
     {
 
-        $request->validate(self::$rules,self::$messages);
-//        $validator = Validator::make($request->all(), [
-//            'title' => 'required|string|unique:articles|max:255',
-//            'body' => 'required|string'
-//        ]);
-//        if ($validator->fails()) {
-//            return response()->json(['error' => 'data_validation_failed', "error_list" => $validator->errors()], 400);
-//        }
+        $request->validate([
+            'title' => 'required|string|unique:articles|max:255',
+            'body' => 'required',
+            'category_id' => 'required|exists:categories,id'
+        ], self::$messages);
 
         $article = Article::create($request->all());
         return response()->json($article, 201);
@@ -46,6 +45,11 @@ class ArticleController extends Controller
 
     public function update(Request $request, Article $article)
     {
+        $request->validate([
+            'title' => 'required|string|unique:articles,title,'.$article->id.'|max:255',
+            'body' => 'required',
+            'category_id' => 'required|exists:categories,id'
+        ], self::$messages);
         $article->update($request->all());
         return response()->json($article, 200);
     }
