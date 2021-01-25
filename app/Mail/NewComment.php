6ba68e9d597ee64e308b8c0c78d7a879cc3b623b
John Vásconez <jovv1994@outlook.com>
2021-01-24 23:51:43 -0500

    #32 Emails

---
+++ app/Mail/NewComment.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class NewComment extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $comment;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param Comment $comment
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.comments.new')
+            ->attach('storage/archivo.pdf', [
+                'as' => 'name.pdf',
+                'mime' => 'application/pdf',
+            ]);
+    }
+}
