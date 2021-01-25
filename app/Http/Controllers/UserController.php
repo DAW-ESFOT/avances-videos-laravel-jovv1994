a99f7f115c33ef38380616e3827a96cdfc895bce
John Vásconez <jovv1994@outlook.com>
2021-01-24 20:33:20 -0500

    #30 Relaciones polimórficas 1 y 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use App\Writer;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use App\Http\Resources\User as UserResource;
 
 class UserController extends Controller
 {
@@ -31,19 +33,26 @@ class UserController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'editorial' => 'required|string',
+            'short_bio' => 'required|string',
         ]);
         if ($validator->fails()) {
             return response()->json($validator->errors()->toJson(), 400);
         }
-        $user = User::create([
+        $writer = Writer::create([
+            'editorial' => $request->get('editorial'),
+            'short_bio' => $request->get('short_bio'),
+        ]);
+        $writer->user()->create([
             'name' => $request->get('name'),
             'email' => $request->get('email'),
             'password' => Hash::make($request->get('password')),
         ]);
+        $user = $writer->user;
 
-        $token = JWTAuth::fromUser($user);
+        $token = JWTAuth::fromUser($writer->user);
 
-        return response()->json(compact('user', 'token'), 201);
+        return response()->json(new UserResource($user, $token), 201);
     }
 
     public function getAuthenticatedUser()
@@ -59,6 +68,6 @@ class UserController extends Controller
         } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
             return response()->json(['token_absent'], $e->getStatusCode());
         }
-        return response()->json(compact('user'));
+        return response()->json(new UserResource($user), 200);
     }
 }
